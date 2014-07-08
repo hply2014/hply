3e98db34a3caa0c343745e4b0b0ea84341299e70
working <hply2014@126.com>
2014-07-08 18:52:59 +0800

    增加mybatis全局配置文件
    Signed-off-by: working <hply2014@126.com>

--- 05_编码/src/hply-webapp/src/test/java/hply/webapp/T3ServiceTest.java
+++ 05_编码/src/hply-webapp/src/test/java/hply/webapp/T3ServiceTest.java
@@ -1,22 +1,21 @@
 package hply.webapp;
 
-import hply.service.T3Service;
+import hply.service.SysUserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @ContextConfiguration(locations = { "classpath:**/applicationContext.xml" })
 public class T3ServiceTest extends AbstractTestNGSpringContextTests {
 	
 	@Autowired
-	private T3Service t3Service;
+	private SysUserService sysUserService;
 
 	@Test
 	public void hasMatchUser() {
-		boolean b = t3Service.hasMatchUser();
-		Assert.assertEquals(true, b);
+		System.out.println("sysUserService.getAll().size()="
+				+ sysUserService.getAll().size());
 	}
 }
