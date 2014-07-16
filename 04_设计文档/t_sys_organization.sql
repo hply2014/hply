b44dab79c300321e1a4178f6cb568c7d425b4e78
working <hply2014@126.com>
2014-07-16 08:49:55 +0800

    数据库设计终稿，所有的表，字段及注释，不包括存储过程和函数
    Signed-off-by: working <hply2014@126.com>

--- 04_设计文档/t_sys_organization.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: SELECT * FROM hplydb_new.t_sys_organization
-LIMIT 0, 1000
-
--- Date: 2014-07-12 22:48
-*/
-INSERT INTO `t_sys_organization` (`ID`,`OrganizationName`,`CreateTime`,`Description`) VALUES ('4ba70b9f-06a2-11e4-99f8-6cf049046039','事业部','2014-07-08 21:17:55',NULL);
-INSERT INTO `t_sys_organization` (`ID`,`OrganizationName`,`CreateTime`,`Description`) VALUES ('4baa1844-06a2-11e4-99f8-6cf049046039','项目部','2014-07-08 21:17:55',NULL);
-INSERT INTO `t_sys_organization` (`ID`,`OrganizationName`,`CreateTime`,`Description`) VALUES ('4babc978-06a2-11e4-99f8-6cf049046039','财务部','2014-07-08 21:17:55',NULL);
--- 04_设计文档/t_sys_parameter.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: SELECT * FROM hplydb_new.t_sys_parameter
-LIMIT 0, 1000
-
--- Date: 2014-07-12 22:48
-*/
-INSERT INTO `t_sys_parameter` (`ID`,`ENName`,`CNName`,`ParamValue`,`DefaultValue`,`ISInner`,`CreateTime`,`CreateUser`,`UpdateTime`,`UpdateUser`,`Description`) VALUES ('2f0a2f68-097f-11e4-a5cb-080027dbb782','appname','应用名称','工程项目核算系统','工程项目核算系统',1,'2014-07-12 12:55:06',NULL,'2014-07-12 12:55:06',NULL,NULL);
-INSERT INTO `t_sys_parameter` (`ID`,`ENName`,`CNName`,`ParamValue`,`DefaultValue`,`ISInner`,`CreateTime`,`CreateUser`,`UpdateTime`,`UpdateUser`,`Description`) VALUES ('4fe8fe7f-097f-11e4-a5cb-080027dbb782','appversion','应用版本号','V1.0','V1.0',1,'2014-07-12 12:56:02',NULL,'2014-07-12 12:56:02',NULL,NULL);
-INSERT INTO `t_sys_parameter` (`ID`,`ENName`,`CNName`,`ParamValue`,`DefaultValue`,`ISInner`,`CreateTime`,`CreateUser`,`UpdateTime`,`UpdateUser`,`Description`) VALUES ('6e053860-097f-11e4-a5cb-080027dbb782','apppublishtime','应用发布时间','2014年8月12日','2014年8月12日',1,'2014-07-12 12:56:52',NULL,'2014-07-12 12:56:52',NULL,NULL);
